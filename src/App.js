f64cf958a08479f2119626cd62e7593b64aeb1a1
bumjuni <darlian0889@gmail.com>
2025-01-06 21:12:02 +0900

    feat: 검색 페이지 퍼블리싱

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import TravelRoutes from './pages/TravelRoutesPage';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import SearchPage from './pages/SearchPage';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/travel-routes" element={<TravelRoutes />} />
+        <Route path="/search" element={<SearchPage />} />
       </Routes>
       <Footer />  
     </BrowserRouter>
