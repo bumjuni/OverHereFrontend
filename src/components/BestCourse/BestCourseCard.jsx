45be7642a810c67aa706700cec3196c5808cc265
bumjuni <darlian0889@gmail.com>
2025-02-05 15:06:17 +0900

    feat: 베스트 코스 카드 및 페이지 구현

---
+++ src/components/BestCourse/BestCourseCard.jsx
@@ -0,0 +1,35 @@
+import dummyImg from '../../assets/image/dummy/dummy_img4.jpg';
+import Badge from '../common/Badge';
+import { ReactComponent as RouteIcon } from "../../assets/image/Route_Pictogram.svg";
+import './BestCourseCard.css';
+
+// RouteIcon svg설정 수정.. current로 변경
+
+function BestCourseCard({rank, img, name}){
+    return (
+        <div className="best-card">
+            <div>
+                <div className="rank">
+                    Best <br /> {rank}
+                </div>
+                <img src={img} alt={name} className="best-image" height="218" width="382" />
+            </div>
+            <div className="rec-contents">
+                <Badge text="지역" color="gray" />
+                <Badge text="코스 설명" color="yellow" />
+                <h2>텍스트</h2>
+                <p>코스 설명글 코스 설명글 코스 설명글 코스 설명글 코스 설명글 코스 설명글 코스 설명글 코스 설명글 코스 설명글 코스 설명글 코스 설명글 </p>
+                <div className="best-badges">
+                    <Badge text="관광지 이름" color="gray" />
+                    <Badge text="관광지 이름" color="gray" />
+                    <Badge text="관광지 이름" color="gray" />
+                    <Badge text="관광지 이름" color="gray" />
+                    <Badge text="관광지 이름" color="gray" />
+                </div>
+                <p className="totalKM"><RouteIcon /> 총 00KM  </p>
+            </div>
+        </div>
+    );
+}
+
+export default BestCourseCard;
