dac91196d7a3db1068aa639f925c50780dd421fc
bumjuni <darlian0889@gmail.com>
2025-02-25 23:10:28 +0900

    feat: API연결 및 더미 이미지 적용 완료

--- src/components/BestCourse/BestCourseCard.jsx
+++ src/components/BestCourse/BestCourseCard.jsx
@@ -4,32 +4,35 @@ import Badge from '../common/Badge';
 import {ReactComponent as TotalDist} from '../../assets/svg/TotalDistance.svg';
 
 
-function BestCourseCard({rank, img, name}){
+function BestCourseCard(props){
     return (
         <>
             <Card>
                 <div>
-                    {rank && 
-                        <Rank>
-                            Best <br /> {rank}
-                        </Rank>
+                    {props.rank && 
+                    <Rank>
+                        Best <br /> {props.rank}
+                    </Rank>
                     }
-                        <img src={img} alt={name} />
-                    </div>
+                    {props.img?
+                     <Image src={props.img} alt={props.title} />
+                     : <DummyImage />
+                    }
+                </div>
 
                 <Content>
-                    <Badge text="지역" color="gray" />
-                    <Badge text="코스 설명" color="yellow" />
-                    <h2>텍스트</h2>
-                    <p>코스 설명글 코스 설명글 코스 설명글 코스 설명글 코스 설명글 코스 설명글 코스 설명글 코스 설명글 코스 설명글 코스 설명글 코스 설명글 </p>
-                    <div className="best-badges">
+                    <Badge text="지역" color="gray" />      {/* 백엔드 부재 */}
+                    <Badge text={props.courseType} color="yellow" />
+                    <h2>{props.title}</h2>
+                    <p>{props.description}</p>
+                    <div className="best-badges">           {/* 백엔드 부재 */}
                         <Badge text="관광지 이름" color="gray" />
                         <Badge text="관광지 이름" color="gray" />
                         <Badge text="관광지 이름" color="gray" />
                         <Badge text="관광지 이름" color="gray" />
                         <Badge text="관광지 이름" color="gray" />
                     </div>
-                    <TotalKM><TotalDist /> 총 00KM  </TotalKM>
+                    <TotalKM> <TotalDist /> 총 {props.distance}KM </TotalKM> 
                 </Content>
             </Card>
             <Divider />
@@ -43,12 +46,13 @@ export default BestCourseCard;
 const Card = styled.div`
     display: flex;
     justify-content: center;
-    img {
-        height: 14em;
-        aspect-ratio: 1 / 0.6;
-        object-fit: cover;
-        border-radius: 5px;
-    }
+`
+const Image = styled.img`
+    height: 14em;
+    aspect-ratio: 1 / 0.6;
+    object-fit: cover;
+    border-radius: 5px;
+    background-color: #ECEEF0;
 `
 const Rank = styled.div`
     position: absolute;
@@ -75,11 +79,16 @@ const TotalKM = styled.p`
 const Content = styled.div`
     position: relative;
     padding: 0 1.5em;
+    width: 100%;
     h2 {
         margin: 0.5em 0;
+    }
+    p {
+        text-overflow: ellipsis;
     }
 `
 const Divider = styled.div`
     border: 1px solid #D4D8DC;
     margin: 2.5em 0;
 `
+const DummyImage = () => <Image as="div" />
--- src/pages/BestCoursePage.jsx
+++ src/pages/BestCoursePage.jsx
@@ -1,5 +1,6 @@
-import {react, useEffect} from 'react';
+import {react, useState, useEffect} from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 import dummy1 from '../assets/image/dummy/dummy_img1.jpg';
 import dummy2 from '../assets/image/dummy/dummy_img2.jpg';
 import dummy3 from '../assets/image/dummy/dummy_img3.jpg';
@@ -10,37 +11,52 @@ import dummy7 from '../assets/image/dummy/dummy_img7.jpg';
 import BestCourseCard from '../components/BestCourse/BestCourseCard';
 import MoreContentsButton from '../components/common/MoreContentsButton';
 
+const initData = [
+    {
+      "courseId": 0,
+      "courseType": "string",
+      "title": "string",
+      "briefDescription": "stringstringstringstringstringstringstringstringstrstringingstringstststringstrstringingstringstststringstrstringingstringstststringstrstringingstringstststringstrstringingstringstststringstrstringingstringstststringstrstringingstringstststringstrstringingstringstststringstrstringingstringstststringstrstringingstringstststringstrstringingstringstststringstrstringingstringstststringstrstringingstringstststringstrstringingstringstststringstrstringingstringstststringstrstringingstringstststringstrstringingstringststringstringstringstringstringstringstringringstringstringstringstringstringstring",
+      "overView": "string",
+      "difficulty": "string",
+      "distance": 0,
+      "img": dummy1
+    }, {
+        "courseId": 0,
+        "courseType": "string",
+        "title": "string",
+        "briefDescription": "string",
+        "overView": "string",
+        "difficulty": "string",
+        "distance": 0,
+    }
+];
 
 function BestCourse(){
-    const result = [
-    {
-        rank: '01',
-        img: dummy1,
-        name: 'first'
-    },
-    {
-        rank: '02',
-        img: dummy2,
-        name: 'second'
-    },{
-        rank: '03',
-        img: dummy3,
-        name: 'third'
-    }
-    ];
+    const [data, setData] = useState(initData);
+    // 처음에 백엔드에서 30개 다 넘겨주고 더보기 버튼 누르면 API호출 없이 랜더링만
+    useEffect(() => {
+        axios.get(`/api/v1/course/best`)
+            .then(res => setData(res.data))
+            .catch(err => alert("베스트 코스 데이터를 가져오는데 실패했습니다."));
+    }, [])
 
     return (
         <>
             <h1>여기너머의 인기 코스</h1> 
             <StyledUl>
-                {result.map(course => 
+                {data.map((item, index) => 
                     <li>
                     <BestCourseCard
-                        rank={course.rank}
-                        img={course.img}
-                        name={course.name}
+                        courseId={item.courseId}
+                        courseType={item.courseType}
+                        title={item.title}
+                        description={item.briefDescription}
+                        distance={item.distance}
+                        rank={index+1}    // 데이터 순서 => rank
+                        img={item.img}
                     />
-                </li>
+                    </li>
                 )}
             </StyledUl>
             <MoreContentsButton />
@@ -55,6 +71,7 @@ const StyledUl = styled.ul`
     flex-direction: column;
     justify-content: center;
     list-style-type: none;
+    word-break: break-word;
     padding: 0;
     margin: 3em 20px 20px 20px;
     `;
